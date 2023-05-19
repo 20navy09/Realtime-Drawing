3c8bff5f5f7821d4fa5e4c154f80cb1770eda3f7
20navy09 <125676898+20navy09@users.noreply.github.com>
2023-05-19 16:06:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+x = 0;
+y = 0;
+screen_width = 0;
+screen_height = 0;
+apple = "";
+speak_data = "";
+to_number = "";
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function preload(){
+  apple = loadImage(apple.png)
+}
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+console.log(event); 
+ content = event.results[0][0].transcript;
+document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+to_number = Number(content);
+if(Number.isInteger(to_number)){
+document.getElementById("status").innerHTML = "Starting drawing an apple";
+draw_apple = "set";
+}
+else{
+  document.getElementById("status").innerHTML = "The speech has not recognised a number";
+  }
+}
+
+function setup() {
+ screen_width = window.innerWidth;
+ screen_height = window.innerHeight;
+
+ canvas = createCanvas(screen_width,screen_height-150);
+canvas.position(0,150);
+}
+
+
